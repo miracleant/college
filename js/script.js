bf484453864536b31599892f7a34ab0249c08ad7
miracleant <chengfeng_hu@sina.cn>
2017-07-21 21:35:12 +0800

    hava some bug in login.html and register.html

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
  * 左边导航栏，显示专业列表下面的子选项
  * @param list
  */
- function showlist(list) {
+ function showList(list) {
             try{
                if(document.getElementById('majorInstruct').style.display!= "block"){
                    document.getElementById('majorInstruct').style.display="block";
@@ -15,4 +15,53 @@
             }catch (e){
                 console.log(e);
             }
+ }
+
+/*login.html js控制*/
+/**
+ * 处理登录操作
+ */
+document.getElementById("login-submit").onclick=function(){
+    var request=new XMLHttpRequest();
+    request.open('GET'," ");
+    request.send();
+    request.onreadystatechange=function() {
+        if (request.readyState === 4) {
+            if (request.status === 200) {
+
+            } else {
+                alert('something error!')
+            }
         }
+    }
+    alert("submit successful!")
+}
+
+/**
+ * 点击注册，跳转到注册页面
+ */
+document.getElementById("login-regist").onclick=function(){
+    document.location.assign("register.html")
+    /*window.location.href="register.html";*/
+}
+
+
+/*register.html js控制*/
+/**
+ * 处理提交注册的操作
+ */
+document.getElementById('regist-submit').onclick=function(){
+    var request=new XMLHttpRequest();
+    request.open('GET'," ");
+    request.send();
+    request.onreadystatechange=function(){
+        if(request.readyState===4){
+            if(request.status===200){
+
+            }else {
+                alert('something error!')
+            }
+        }
+    }
+    alert("submit successful");
+}
